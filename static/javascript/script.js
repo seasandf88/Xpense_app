2307fa57c276428f95769a35eb638a62035f9bf4
Fahed Nassar <engr.f.nassar@gmail.com>
2023-10-16 14:48:25 +0300

    Added login and sign up dialog

---
+++ static/javascript/script.js
@@ -0,0 +1,24 @@
+// Getting the dialog elements from the DOM
+const loginBtn = document.getElementById("login-btn");
+const loginDialog = document.getElementById("login-dialog");
+const loginCancel = document.querySelectorAll(".cancel");
+const tabs = document.querySelectorAll(".tab")
+const forms = document.querySelectorAll(".form")
+
+// Open the Dialog using a built-in method showModal()
+loginBtn.addEventListener("click", () => loginDialog.showModal());
+
+// Close the Dialog using a built-in method close()
+loginCancel.forEach((btn) => {
+  btn.addEventListener("click", () => loginDialog.close());
+});
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(tab => tab.classList.remove("active-tab"))
+    tab.classList.add("active-tab")
+    forms.forEach(form => form.classList.remove("active-form"))
+    
+    forms[index].classList.add("active-form")
+  })
+})
